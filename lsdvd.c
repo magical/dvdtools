1ca15934e7eeb20e986bbd3216c52137e771c9b5
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2013-03-12 18:10:09 -0700

    Refactoring

--- lsdvd.c
+++ lsdvd.c
@@ -3,11 +3,11 @@
 #include <dvdread/dvd_reader.h>
 #include <dvdread/ifo_read.h>
 
-#define BLOCK_SIZE DVD_VIDEO_LB_LEN
-
 typedef unsigned int uint;
 typedef unsigned char u8;
 typedef unsigned long long u64;
+
+typedef u8 sectorbuf[DVD_VIDEO_LB_LEN];
 
 void die(char *s) {
 	fputs(s, stderr);
@@ -62,7 +62,7 @@ int get_scr(dvd_reader_t *dvd, int title, int sector, u64 *scrp)
 {
 	dvd_file_t *vob = DVDOpenFile(dvd, title, DVD_READ_TITLE_VOBS);
 	if (vob == NULL) { return -1; }
-	u8 b[BLOCK_SIZE];
+	sectorbuf b;
 	if (DVDReadBlocks(vob, sector, 1, b) < 0) { goto error; }
 	if (b[0] != 0 || b[1] != 0 || b[2] != 1 || b[3] != 0xba) {
 		printf("get_scr: %d,%d: %02x%02x%02x%02x\n", title, sector, b[0], b[1], b[2], b[3]);
@@ -75,8 +75,8 @@ int get_scr(dvd_reader_t *dvd, int title, int sector, u64 *scrp)
 	          ((b[6] & 3) << 13) |
 	          (b[7] << 5) |
 	          (b[8] >> 3);
-	uint scr_ext = ((b[8] & 3) << 7) |
-	               (b[9] >> 1);
+	uint scr_ext = (((uint)b[8] & 3) << 7) |
+	               ((uint)b[9] >> 1);
 	scr = scr * 300 + scr_ext;
 	*scrp = scr;
 	DVDCloseFile(vob);
@@ -86,21 +86,72 @@ error:
 	return -1;
 }
 
-int get_pts(dvd_reader_t *dvd, int title, int sector, u64 *ptsp, int skip)
+int pack_stream_id(sectorbuf b)
+{
+	// Theoretically, each pack only contains a single stream, so we
+	// shouldn't have to look beyond the first packet.
+	if (b[0] != 0 || b[1] != 0 || b[2] != 1 || b[3] != 0xba) {
+		return -1; // not a PACK
+	}
+	u8 *p = b + 0xe + (b[0xd] & 7); // stuffing
+	if (p[0] != 0 || p[1] != 0 || p[2] != 1) {
+		return -1; // not a packet
+	}
+	if (p[3] == 0xbd) {
+		// private stream
+		return p[9+p[8]];
+	}
+	return p[3];
+}
+
+// Return the sector of the first audio packet for the given audio index.
+// The sector argument should be the sector of a NAV packet.
+int get_audio_sector(dvd_file_t *vob, int sector, int index)
+{
+	sectorbuf b;
+	if (DVDReadBlocks(vob, sector, 1, b) < 1) { return -1; }
+	if (pack_stream_id(b) != 0xbb) {
+		// Not a NAV
+		printf("not a nav: %#x\n", pack_stream_id(b));
+		return -1;
+	}
+	// Read the a_async value for the requested stream.
+	// Should probably just use nav_read.c.
+	u8 *p = b + 0x599 + index*2;
+	int n = (p[0] & 0x7f) << 8 | p[1];
+	if (p[0] & 0x80) {
+		return sector - n;
+	}
+	return sector + n;
+}
+
+#define MAX_TRIES 10
+
+// Scan for the first packet with the given stream id, starting at sector.
+// Returns -1 if the stream is not found within the maximum number of tries,
+// or DVDReadBlock fails.
+int find_stream(dvd_file_t *vob, int sector, int stream)
+{
+	sectorbuf b;
+	for (int try = 0; try < MAX_TRIES; try++) {
+		if (DVDReadBlocks(vob, sector+try, 1, b) < 1) return -1;
+		if (pack_stream_id(b) == stream) {
+			return try;
+		}
+	}
+	return -1;
+}
+
+int get_pts(dvd_reader_t *dvd, int title, int sector, u64 *ptsp)
 {
 	dvd_file_t *vob = DVDOpenFile(dvd, title, DVD_READ_TITLE_VOBS);
 	if (vob == NULL) { return -1; }
 
-	u8 b[BLOCK_SIZE];
-	int async = 0;
-	if (skip) {
-		if (DVDReadBlocks(vob, sector, 1, b) < 1) { goto error; }
-		async = b[0x59a];
-	}
+	sectorbuf b;
 	for (int try = 0; try < 150; try++) {
-		if (DVDReadBlocks(vob, sector+async+try, 1, b) < 1) { goto error; }
+		if (DVDReadBlocks(vob, sector+try, 1, b) < 1) { goto error; }
 		if (b[0] != 0 || b[1] != 0 || b[2] != 1 || b[3] != 0xba) {
-			printf("get_pts: %d,%d: %02x%02x%02x%02x\n", title, sector+async+try, b[0], b[1], b[2], b[3]);
+			printf("get_pts: %d,%d: %02x%02x%02x%02x\n", title, sector+try, b[0], b[1], b[2], b[3]);
 			goto error;
 		}
 		if (b[14] != 0 || b[15] != 0 || b[16] != 1 || b[17] != 0xbd ||
@@ -111,7 +162,7 @@ int get_pts(dvd_reader_t *dvd, int title, int sector, u64 *ptsp, int skip)
 		if (!((0x80 <= code && code < 0x90) || (0xa0 <= code && code < 0xa8))) {
 			continue;
 		}
-		printf("found %#02x at %d+%d+%d\n", code, sector, async, try);
+		printf("found %#02x at %d+%d\n", code, sector, try);
 		u64 pts = ((u64)(b[23] & 0x0e) >> 1 << 30) |
 		          ((u64)b[24] << 22) |
 		          ((u64)b[25] >> 1 << 15) |
@@ -153,6 +204,11 @@ int main(int argc, char *argv[])
 		if (ifo == NULL) {
 			continue;
 		}
+		dvd_file_t *vob = DVDOpenFile(dvd, title, DVD_READ_TITLE_VOBS);
+		if (vob == NULL) {
+			ifoClose(ifo);
+			continue;
+		}
 
 		vtsi_mat_t *v = ifo->vtsi_mat;
 		for (int j = 0; j < v->nr_of_vts_audio_streams; j++) {
@@ -168,10 +224,12 @@ int main(int argc, char *argv[])
 				/*printf("%d,%d: %d\n", j, k,
 					1 + pb->last_sector - pb->first_sector);
 				*/
+				int audio_sector = get_audio_sector(vob, pb->first_sector, 0);
+				if (audio_sector < 0) continue;
 				u64 scr, pts0, pts;
 				if (get_scr(dvd, title, pb->first_sector, &scr) >= 0 &&
-				    get_pts(dvd, title, pb->first_sector, &pts0, 0) >= 0 &&
-				    get_pts(dvd, title, pb->first_sector, &pts, 1) >= 0) {
+				    get_pts(dvd, title, pb->first_sector, &pts0) >= 0 &&
+				    get_pts(dvd, title, audio_sector, &pts) >= 0) {
 					struct time t = time_from_scr(scr);
 					struct time len = time_from_scr(scr - last_scr);
 					printf("%d,%d: %llu ", j, k, scr);
@@ -187,7 +245,7 @@ int main(int argc, char *argv[])
 				}
 			}
 		}
-
+		DVDCloseFile(vob);
 		ifoClose(ifo);
 	}
 	ifoClose(ifo0);
